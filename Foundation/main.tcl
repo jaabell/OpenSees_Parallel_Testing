7d56a6a19c44f5e61002ac1102b0bc362213e63b
Jose Abell <ja.abell@gmail.com>
2018-07-03 12:45:42 -0400

    disable element level outputs

--- Foundation/main.tcl
+++ Foundation/main.tcl
@@ -46,8 +46,8 @@ source "./model_foundation_${MESHSIZE}/foundation_${MESHSIZE}.elements.tcl"
 
 
 # recorder gmsh output disp
-recorder gmsh timing updatetime eleupdatetime
-# recorder gmsh timing updatetime 
+# recorder gmsh timing updatetime eleupdatetime
+recorder gmsh timing updatetime 
 # recorder gmsh eleoutput eleResponse updatetime
 
 recorder Node -file "nodedisp.out" -time -node 1 2 3 4 -dof 1 2 3 disp
